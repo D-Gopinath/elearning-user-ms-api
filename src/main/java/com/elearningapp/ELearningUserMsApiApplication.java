216bb92055be858ba45834608a546dd99486d379
D-Gopinath <gopinathd3737@gmail.com>
2022-05-05 18:09:08 +0530

    first commit

---
+++ src/main/java/com/elearningapp/ELearningUserMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.elearningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ELearningUserMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningUserMsApiApplication.class, args);
+	}
+
+}
